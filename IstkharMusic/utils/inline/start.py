94287de89af741dbf55b56ec94532d8474c09874
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2026-08-23 09:26:11 +0530

    Update start.py

--- IstkharMusic/utils/inline/start.py
+++ IstkharMusic/utils/inline/start.py
@@ -34,5 +34,9 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style=ButtonStyle.SUCCESS),
             InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/TuneBots", style=ButtonStyle.SUCCESS),
         ],
+        [
+            InlineKeyboardButton(text="🥀 ʀᴇᴘᴏ", url="\x68\x74\x74\x70\x73\x3a\x2f\x2f\x67\x69\x74\x68\x75\x62\x2e\x63\x6f\x6d\x2f\x54\x45\x41\x4d\x2d\x49\x53\x54\x4b\x48\x41\x52\x2f\x49\x73\x74\x6b\x68\x61\x72\x4d\x75\x73\x69\x63", style=ButtonStyle.SUCCESS)
+        ]
     ]
     return buttons
+    
