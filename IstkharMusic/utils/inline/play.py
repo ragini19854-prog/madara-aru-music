9e3f0ba399c4c62f8a9f0755f2a27c576a5c7da1
s88564264-art <s88564264@gmail.com>
2026-08-25 20:17:29 +0530

    Update play.py

--- IstkharMusic/utils/inline/play.py
+++ IstkharMusic/utils/inline/play.py
@@ -43,11 +43,11 @@ def stream_markup_timer(_, chat_id, played, dur):
 
     buttons = [
         [
-            InlineKeyboardButton(text="❤️‍🔥", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="🪼", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="🪫", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="👻", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="♦️", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="❙ ❙", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↺", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="➔|", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
@@ -65,11 +65,11 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="❤️‍🔥", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="🪼", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="🪫", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="👻", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="♦️", callback_data=f"ADMIN Stop|{chat_id}")
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="❙ ❙", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↺", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="➔|", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
