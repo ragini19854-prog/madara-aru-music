da460219297db07317254e7f4f4fb49e0c3cb7b8
s88564264-art <s88564264@gmail.com>
2026-08-25 19:46:15 +0530

    Update start.py

--- IstkharMusic/utils/inline/start.py
+++ IstkharMusic/utils/inline/start.py
@@ -1,9 +1,6 @@
 from pyrogram.types import InlineKeyboardButton
-from pyrogram.enums import ButtonStyle
 import config
 from IstkharMusic import app
-
-
 def start_panel(_):
     buttons = [
         [
@@ -14,29 +11,25 @@ def start_panel(_):
         ],
     ]
     return buttons
-
-
 def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.DANGER,
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=ButtonStyle.SUCCESS),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP, style=ButtonStyle.SUCCESS),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style=ButtonStyle.SUCCESS),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/TuneBots", style=ButtonStyle.SUCCESS),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/TuneBots"),
         ],
         [
-            InlineKeyboardButton(text="🥀 ʀᴇᴘᴏ", url="\x68\x74\x74\x70\x73\x3a\x2f\x2f\x67\x69\x74\x68\x75\x62\x2e\x63\x6f\x6d\x2f\x54\x45\x41\x4d\x2d\x49\x53\x54\x4b\x48\x41\x52\x2f\x49\x73\x74\x6b\x68\x61\x72\x4d\x75\x73\x69\x63", style=ButtonStyle.SUCCESS)
+            InlineKeyboardButton(text="🥀 ʀᴇᴘᴏ", url="\x68\x74\x74\x70\x73\x3a\x2f\x2f\x67\x69\x74\x68\x75\x62\x2e\x63\x6f\x6d\x2f\x54\x45\x41\x4d\x2d\x49\x53\x54\x4b\x48\x41\x52\x2f\x49\x73\x74\x6b\x68\x61\x72\x4d\x75\x73\x69\x63")
         ]
     ]
     return buttons
-    
